7725669a7283a36c4286216f3fb4f274f43bdb31
SHwen <919836750@qq.com>
2023-06-19 16:30:41 +0800

    修改embedding模型

--- configs/model_config.py
+++ configs/model_config.py
@@ -12,7 +12,7 @@ logging.basicConfig(format=LOG_FORMAT)
 embedding_model_dict = {
     #"ernie-tiny": "nghuyong/ernie-3.0-nano-zh",
     #"ernie-base": "nghuyong/ernie-3.0-base-zh",
-    "text2vec-base": "/home/text2vec-base-chinese",
+    #"text2vec-base": "/home/text2vec-base-chinese",
     "text2vec": "/home/text2vec-large-chinese",
 }
 
